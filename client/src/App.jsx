9ec91dde64bb6048ac3cfb37e353dd2d2d90c179
ljsimpkin@gmail.com <sandbox1@Sands-MacBook-Pro.local>
2022-08-18 01:36:53 +1200

    add cors for github

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,8 @@ import Drafting from "./Drafting"
 import Voting from "./Voting"
 import Results from "./Results"
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://votal.herokuapp.com");
+// const socket = io.connect("http://localhost:3001");
 
 function App() {
   //Room State
--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://127.0.0.1:5173",
+    origin: ["http://127.0.0.1:5173", "https://ljsimpkin.github.io/votey/"],
     methods: ["GET", "POST"],
   },
 });
