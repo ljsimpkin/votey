269f7e294a503636d0ad25a9d4c2ce76acc10f64
ljsimpkin@gmail.com <sandbox1@Sands-MacBook-Pro.local>
2022-08-18 01:51:55 +1200

    cors

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://127.0.0.1:5173", "https://ljsimpkin.github.io/votey"],
+    // origin: ["http://127.0.0.1:5173", "https://ljsimpkin.github.io/votey"],
+    origin: "https://ljsimpkin.github.io/votey",
     methods: ["GET", "POST"],
   },
 });
