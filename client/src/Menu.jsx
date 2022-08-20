b1fd699777ae1aa6bd2a2432baf78983dc7909d3
ljsimpkin@gmail.com <sandbox1@Sands-MacBook-Pro.local>
2022-08-20 23:39:57 +1200

    Menu styled

--- client/src/Menu.jsx
+++ client/src/Menu.jsx
@@ -2,18 +2,31 @@ function Menu(props) {
   const {setRoom, joinRoom} = props.state
 
   return (
-    <div className="App">
-      <>
-        <h1>Create or join a game</h1>
+    <div className="Menu">
 
-        <input
-            placeholder="Room Number..."
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-          />
-          <button onClick={joinRoom}> Join Room</button>
-      </>
+        <h1 className="menuTitle">Votey</h1>
+        <h2 className="menuSubTitle">A party game voting app</h2>
+        
+        <div className="gameCodeTextBoxDiv">
+          <input
+              className="gameCodeTextBox"
+              placeholder="Game code"
+              onChange={(event) => {
+                setRoom(event.target.value);
+              }}
+            />
+        </div>
+
+        <button className="buttons joinButton" onClick={joinRoom}>Join</button>
+        
+        <div className="createButtonDiv">
+          <button className="buttons" >Create</button>
+        </div>
+        
+
+        <h3 className="footerMessage">Made enthusiastically 
+by Liam Simpkin 🔨</h3>
+
     </div>
   );
 }
